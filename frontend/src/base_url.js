191a94672daf87925ad97e4f304c4cda6d75915b
darshan1301 <mahajandarshan76@gmail.com>
2024-03-14 01:47:14 +0530

    deployed BE to the aws.

--- frontend/src/base_url.js
+++ frontend/src/base_url.js
@@ -1,4 +1,6 @@
-const base_url = "https://hotel-booking-app-tytz.onrender.com";
+const base_url =
+  "http://hotel-booking-version-two-env.eba-37pvvkhv.ap-south-1.elasticbeanstalk.com";
+// const base_url = "https://hotel-booking-app-tytz.onrender.com";
 // const base_url = "http://localhost:8000";
 
 export default base_url;
